44c92c5726a8d3b3db2388ff617f4ca5f87a3ab3
Luc Calaresu <dev@calaresu.com>
2023-03-05 17:48:41 +0100

    feat(read): Update the ReadProgramCommand
    The command has been updated in order to read all the file and construct
    all needed obkects.

--- app/src/main/java/com/publicisgroupe/lawnmower/models/LawnmowerInitRecord.java
+++ app/src/main/java/com/publicisgroupe/lawnmower/models/LawnmowerInitRecord.java
@@ -2,18 +2,14 @@ package com.publicisgroupe.lawnmower.models;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.util.List;
-
 /**
  * Record to store the start value of a given Lawnmower.
  *
  * @param startX           the X coordinate of the lawnmower at the start
  * @param startY           the Y coordinate of the lawnmower at the start
  * @param startOrientation the orientation of the lawnmower at the start
- * @param instructions     all the instructions of the lawnmower
  */
 public record LawnmowerInitRecord(int startX,
                                   int startY,
-                                  @NotNull LawnmowerOrientation startOrientation,
-                                  @NotNull List<Character> instructions) {
+                                  @NotNull LawnmowerOrientation startOrientation) {
 }
