0d43f2ff4e7aa0397c7b4db5611d78df3c79c110
Luc Calaresu <dev@calaresu.com>
2023-03-05 17:46:57 +0100

    feat: Added class LawnCoordinate, representing coordinates

---
+++ app/src/main/java/com/publicisgroupe/lawnmower/models/LawnCoordinate.java
@@ -0,0 +1,10 @@
+package com.publicisgroupe.lawnmower.models;
+
+/**
+ * Define a coordinate point for the lawn.
+ *
+ * @param x abscissa of the point
+ * @param y ordinate of the point
+ */
+public record LawnCoordinate(int x, int y) {
+}
